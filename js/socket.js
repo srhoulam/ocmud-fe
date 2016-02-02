0387ae7348ce3c2dc840c65af4b1c05bb8408b31
Saad Rhoulam <saad@rhoulam.com>
2016-02-02 05:59:46 -0500

    Log new event types to console.

--- js/socket.js
+++ js/socket.js
@@ -5,7 +5,13 @@ var socket = {
         var socket = io(url);
 
         socket.on("info", ui.methods.addToLog);
+        socket.on("error", ui.methods.addToLog);
+
         socket.on("numClients", num => console.log("numClients:", num.clients));
+
+        socket.on("travel", ui.methods.addToLog);
+        socket.on("action", ui.methods.addToLog);
+
         socket.on("sight", ui.methods.displaySight);
         socket.on("speech", obj => console.log("Speech:", `<${obj.from}> ${obj.message}`));
 
