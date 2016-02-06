e8ffac385aa6fa960be16b775782d2879e3f5d06
Saad Rhoulam <saad@rhoulam.com>
2016-02-05 19:39:07 -0500

    Detecting downgrades isn't working. 'ident' listener isn't firing even when teh `api.whoami` call is delayed after setting the handler.

--- js/socket.js
+++ js/socket.js
@@ -14,21 +14,9 @@ var socket = {
 
         socket.on("connect", function() {
             console.log("Connected.");
-
-            socket.on('ident', function(id) {
-                if(id !== app.loggedInAs) {
-                    app.loggedInAs = id;
-
-                    if(id === false) {
-                        console.log("Reconnected and downgraded to explore mode.");
-                    }
-                }
-            });
         });
         socket.on("disconnect", function() {
             console.log("Disconnected.");
-
-            app.loggedInAs = false;
         });
 
         socket.on("info", ui.methods.addToLog);
