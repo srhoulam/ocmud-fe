be2219563a6f0b2611fb9b31330529312f731e01
Saad Rhoulam <saad@rhoulam.com>
2016-02-15 16:33:44 -0500

    Move call to UI init method to DOMContentLoaded handler rather than window load handler.

--- js/index.js
+++ js/index.js
@@ -6,8 +6,5 @@ var app = {
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log("DOMContentLoaded");
-});
-window.addEventListener('load', function() {
-    console.log("Page fully loaded.");
     ui.methods.init();
 });
